e3114b6c6d6c3e76e688e04197c2850e63f1aa07
Jeff Nuyts <NUYJE@sintcordula.be>
2024-10-16 08:57:33 +0200

    Add project files.

---
+++ BIWToetsH3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BIWToetsH3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
